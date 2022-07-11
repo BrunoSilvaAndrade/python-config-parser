5e06ae808eb60123e91b543e4e3df456026f273f
BrunoSilvaAndrade <bruno.andrade@viavarejo.com.br>
2022-07-11 01:02:46 -0300

    Updating README.md

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='python-config-parser',
-    version='2.0.6',
+    version='2.1.0',
     author='Bruno Silva de Andrade',
     author_email='brunojf.andrade@gmail.com',
     description='Project created to given the possibility of create dynamics config files',
