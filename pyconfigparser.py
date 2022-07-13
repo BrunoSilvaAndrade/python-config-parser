c8bd34f0961bf7c7febbf6e65e376c9fcb9d6296
BrunoSilvaAndrade <bruno.andrade@viavarejo.com.br>
2022-07-13 18:32:13 -0300

    fix(api)!: Renaming classes and creating a default package object A class which provides the ConfigValue Object was not a good deal Now there's a default package object called configparser which gives a Config(old ConfigValue) object

--- pyconfigparser.py
+++ pyconfigparser.py
@@ -5,7 +5,7 @@ from os import path
 import os
 import re
 
-LINUX_KEY_VARIABLE_PATTERN = r'\$([a-zA-Z][\w]+|\{[a-zA-Z][\w]+\})$'
+VARIABLE_PATTERN = r'\$([a-zA-Z][\w]+|\{[a-zA-Z][\w]+\})$'
 DEFAULT_CONFIG_FILES = ('config.json', 'config.yaml', 'config.yml')
 ENTITY_NAME_PATTERN = r'^[a-zA-Z][\w]+$'
 SUPPORTED_EXTENSIONS = {
@@ -23,7 +23,7 @@ class ConfigFileNotFoundError(ConfigError):
     pass
 
 
-class ConfigValue:
+class Config:
 
     def __getitem__(self, item):
         return self.__dict__[item]
@@ -41,58 +41,52 @@ class ConfigValue:
         return self.__dict__.values()
 
 
-class Config:
-    __instance = None
-    __hold_an_instance = True
+class configparser:
+    def __init__(self):
+        self.__instance = None
+        self.__hold_an_instance = True
 
-    @classmethod
-    def hold_an_instance(cls):
-        return cls.__hold_an_instance
+    @property
+    def hold_an_instance(self):
+        return self.__hold_an_instance
 
-    @classmethod
-    def set_hold_an_instance(cls, value):
+    @hold_an_instance.setter
+    def hold_an_instance(self, value):
         if type(value) is not bool:
             raise ValueError('value must be a bool')
-        cls.__hold_an_instance = value
+        self.__hold_an_instance = value
 
-    def __new__(cls, *args, **kwargs):
-        raise RuntimeError('A instance of config is not allowed, use Config.get_config() instead')
+    def get_config(self, schema: dict = None, config_dir: str = 'config', file_name: Any = DEFAULT_CONFIG_FILES):
 
-    @classmethod
-    def get_config(cls, schema: dict = None, config_dir: str = 'config', file_name: Any = DEFAULT_CONFIG_FILES):
-
-        if cls.__instance is None:
-            instance = cls.__create_new_instance(schema, config_dir, file_name)
-            if cls.__hold_an_instance:
-                cls.__instance = instance
+        if self.__instance is None:
+            instance = self.__create_new_instance(schema, config_dir, file_name)
+            if self.__hold_an_instance:
+                self.__instance = instance
             else:
                 return instance
-        return cls.__instance
+        return self.__instance
 
-    @classmethod
-    def __create_new_instance(cls, schema, config_dir, file_name):
-        file_path = cls.__get_file_path(config_dir, file_name)
-        parser = cls.__get_file_parser(file_path)
-        file_buff = cls.__get_file_buff(file_path)
+    def __create_new_instance(self, schema, config_dir, file_name):
+        file_path = self.__get_file_path(config_dir, file_name)
+        parser = self.__get_file_parser(file_path)
+        file_buff = self.__get_file_buff(file_path)
 
         try:
-            config = cls.__validate_schema(schema, parser(file_buff))
-            return cls.__dict_2_obj(config)
+            config = self.__validate_schema(schema, parser(file_buff))
+            return self.__dict_2_obj(config)
         except ParseError as e:
             raise ConfigError(e)
         except SchemaError as e:
             raise ConfigError('Schema validation error', e)
 
-    @classmethod
-    def __get_file_parser(cls, file_path):
+    def __get_file_parser(self, file_path):
         try:
             extension = file_path.split('.')[-1]
             return SUPPORTED_EXTENSIONS[extension]
         except KeyError:
             raise ConfigError(f'Supported extensions: {list(SUPPORTED_EXTENSIONS.keys())}')
 
-    @classmethod
-    def __get_file_path(cls, config_dir, file_name):
+    def __get_file_path(self, config_dir, file_name):
         file_path = f'{os.getcwd()}/{config_dir}/'
         if type(file_name) is str:
             file_name = [file_name]
@@ -103,8 +97,7 @@ class Config:
 
         raise ConfigFileNotFoundError(f'Config file {file_path}{file_name} was not found')
 
-    @classmethod
-    def __validate_schema(cls, schema, config_obj):
+    def __validate_schema(self, schema, config_obj):
         if schema is None:
             return config_obj
         elif type(schema) not in (dict, list):
@@ -112,40 +105,44 @@ class Config:
 
         return Schema(schema).validate(config_obj)
 
-    @classmethod
-    def __get_file_buff(cls, path_file: str):
+    def __get_file_buff(self, path_file: str):
         with open(path_file, 'r') as f:
             return f.read()
 
-    @classmethod
-    def __dict_2_obj(cls, data: Any):
+    def __dict_2_obj(self, data: Any):
         _type = type(data)
 
         if _type is dict:
-            obj = ConfigValue()
+            obj = Config()
             for key, value in data.items():
-                if re.search(ENTITY_NAME_PATTERN, key) is None:
-                    raise ConfigError(
-                        f'The key {key} is invalid. The entity keys only may have words, number and underscores.')
-                setattr(obj, key, cls.__dict_2_obj(value))
+                self.__is_a_valid_object_key(key)
+                setattr(obj, key, self.__dict_2_obj(value))
             return obj
         if _type in (list, set, tuple):
-            return list(map(lambda v: cls.__dict_2_obj(v), data))
+            return list(map(lambda v: self.__dict_2_obj(v), data))
         else:
-            if type(data) is str and re.search(LINUX_KEY_VARIABLE_PATTERN, data) is not None:
-                return cls.interpol_variable(data)
+            if self.__is_variable(data):
+                return self.__interpol_variable(data)
             return data
 
-    @classmethod
-    def interpol_variable(cls, data):
+    def __is_a_valid_object_key(self, key):
+        if re.search(ENTITY_NAME_PATTERN, key) is None:
+            raise ConfigError(f'The key {key} is invalid. The entity keys only may have words, number and underscores.')
+
+    def __is_variable(self, data):
+        return type(data) is str and re.search(VARIABLE_PATTERN, data) is not None
+
+    def __interpol_variable(self, data):
         try:
-            return os.environ[cls.extract_env_variable_key(data)]
+            return os.environ[self.__extract_env_variable_key(data)]
         except KeyError:
             raise ConfigError(f'Environment variable {data} was not found')
 
-    @classmethod
-    def extract_env_variable_key(cls, variable):
+    def __extract_env_variable_key(self, variable):
         variable = variable[1:]
         if variable[0] == '{':
             return variable[1:-1]
         return variable
+
+
+configparser = configparser()
